99e2349145918b49f51576420bc93b432b875e2b
Mahamadou Simpara <mahamadousimpara@Simparas-MacBook-Air.local>
2024-08-15 11:21:14 +0000

    NextJS basics how to create route static and dynamic folder structure

---
+++ app/about/[index]/page.js
@@ -0,0 +1,12 @@
+function AbooutNumber({ params }) {
+  return (
+    <main>
+      <div>
+        <h1>This is about survival</h1>
+        <h1>{params.index}</h1>
+      </div>
+    </main>
+  );
+}
+
+export default AbooutNumber;
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+function AboutPage () {
+    return <main>
+        <h1>This is about survival</h1>
+    </main>
+}
+
+
+
+export default AboutPage;
